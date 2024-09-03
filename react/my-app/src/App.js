73840ca527bb6ae56aad908f60008defdf1f8b9d
krleesin <dnjsgh5@naver.com>
2024-09-03 15:46:19 +0900

    figma실행

---
+++ react/my-app/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from "react";
+import { Desktop } from "./components/index";
+import "./components/style.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Desktop />
+    </div>
+  );
+};
+
+export default App; // Ensure this line is present
